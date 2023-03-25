34ec730263ff06251d927ddb608a7da351d09b51
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-03-25 21:45:27 +0700

    add: address, detail product on invoice

--- src/components/manage-transaction/ViewTransaction.jsx
+++ src/components/manage-transaction/ViewTransaction.jsx
@@ -151,13 +151,12 @@ const ViewTransaction = () => {
             </p>
           </div>
         </div>
-
         <div className="mt-4">
           <div class="relative overflow-x-auto">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-fixed">
               <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" class="px-6 py-3">
+                  <th scope="col" class="px-6 py-3 w-4">
                     No
                   </th>
                   <th scope="col" class="px-6 py-3">
@@ -182,7 +181,15 @@ const ViewTransaction = () => {
                       >
                         {index + 1}
                       </th>
-                      <td class="px-6 py-4">{item.varianProduk.namavarian}</td>
+                      <td class="px-6 py-4">
+                        <span className="font-semibold">
+                          {item.varianProduk.namavarian}
+                        </span>{" "}
+                        <br />
+                        <span className="text-xs">
+                          {item.kodeproduk} - IMEI : {item.imei}
+                        </span>
+                      </td>
                       <td class="px-6 py-4">
                         Rp. {item.harga.toLocaleString()}
                       </td>
@@ -208,7 +215,40 @@ const ViewTransaction = () => {
                 </tr>
               </thead>
             </table>
+            <button
+              onClick={() => window.print()}
+              class="px-4 py-2 mt-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-primary border border-transparent rounded-lg"
+              id="btn-print"
+            >
+              <div className="flex flex-row">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 pr-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z"
+                  />
+                </svg>
+                Print
+              </div>
+            </button>
           </div>
+        </div>
+        <div
+          className="py-4 px-4 mt-3 text-sm font-medium tracking-tight hidden text-gray-600 rounded-md md:text-sm"
+          id="alamat"
+        >
+          <hr className="pb-4" />
+          <span className="font-bold">BAGJA CELL</span>
+          <br />
+          Mulyasari, Kec. Bayongbong, Kabupaten Garut, Jawa Barat 44162 <br />
+          082116620263
         </div>
       </div>
     </div>
